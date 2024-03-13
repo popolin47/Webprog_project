061d040913c9c706f50533f72def48e031bb0999
popolin47 <popo2027188@gmail.com>
2024-03-13 21:48:34 +0700

    Mockdata Product

---
+++ stocky/src/ProductData.tsx
@@ -0,0 +1,24 @@
+export const Product = [
+    {
+        pro_name: 'shoes1',
+        catagory: 'Man',
+        brand: 'Adibas',
+        avaliable: 1,
+        size: 'UK35',
+    },
+    {
+        pro_name: 'shoes2',
+        catagory: 'Women',
+        brand: 'Nike',
+        avaliable: 0,
+        size: 'UK36',
+    },
+    {
+        pro_name: 'shoes3',
+        catagory: 'Kid',
+        brand: 'Aria',
+        avaliable: 1,
+        size: 'UK35',
+    },
+    
+];
