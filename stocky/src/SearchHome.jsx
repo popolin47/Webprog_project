8172bdcc1bd072fb376156dc1acce8947447c0d6
popolin47 <popo2027188@gmail.com>
2024-03-14 14:30:45 +0700

    edit searchhome

--- stocky/src/SearchHome.jsx
+++ stocky/src/SearchHome.jsx
@@ -59,14 +59,14 @@ const SearchHome = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form onSubmit={handleSearchSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="searchName">
+    <div className="bg-[#F5F5F5]">
+      <form onSubmit={handleSearchSubmit} >
+        <div >
+          <label  htmlFor="searchName">
             Product Name:
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            
             id="searchName"
             type="text"
             placeholder="Enter product name"
@@ -75,12 +75,12 @@ const SearchHome = () => {
           />
         </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">
+        <div >
+          <label  htmlFor="category">
             Category:
           </label>
           <select
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            
             id="category"
             value={selectedCategory}
             onChange={handleCategoryChange}
