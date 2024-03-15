8420260c724afe90bd8070576d36d4d47bb27694
RollTee <163039525+RollTee@users.noreply.github.com>
2024-03-15 23:27:16 +0700

    Login

---
+++ stocky/src/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleLogin = () => {
+        console.log("Username:", username);
+        console.log("Password:", password);
+
+        history.push('/Home');
+    };
+
+    return (
+        <div className="container">
+            <h2>Login Form</h2>
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input 
+                    type="text" 
+                    id="username" 
+                    placeholder="Enter username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="form-group">
+                <button onClick={handleLogin}>Enter</button>
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Login;
