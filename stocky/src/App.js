7127006a6db2ac56919b329a91e549d94e0e9752
jajayjin@gmail.com <jajayjin@gmail.com>
2024-03-16 21:28:53 +0700

    Navbar

--- stocky/src/App.js
+++ stocky/src/App.js
@@ -5,34 +5,36 @@ import Blogdetails from './Blogdetails';
 import Create from './Create';
 import Home from './Home';
 import Navbarwithsearch from './Navbarwithsearch';
+import Navbarwithoutsearch from './Navbarwithoutsearch';
 import SearchHome from './SearchHome';
 import ProductDetail from './ProductDetail';
-import SearchResultPage from './result_product';
+import { Sidebar } from 'react-pro-sidebar';
+import Aboutus from './Aboutus';
 class App extends React.Component {
   render() {
     const title = "Welcome to login blog";
     const like = 4;
     const person = {name: 'jin', age: 12};
+    
 
+  
+    
     return (
       <Router>
         <div className="App">
-          <Navbarwithsearch />
           <div className="content">
             <Switch>
-
               <Route exact path="/">
+                <Navbarwithsearch/>
                 <Home />
               </Route>
-
-              <Route path="/create">
-                <Create />
+              <Route path="/productdetail">
+                <Navbarwithsearch/>
+                <ProductDetail />
               </Route>
-
               <Route path="/blogs/:id">
                 <Blogdetails/>
               </Route>
-
               <Route path="/blogs/:id">
                 <Blogdetails/>
               </Route>
@@ -42,17 +44,16 @@ class App extends React.Component {
               </Route>
 
               <Route path="/Searchhome">
-                <SearchHome/>
+                <Navbarwithoutsearch/>
+                <SearchHome />
               </Route>
             
               <Route path="/ProductDetail">
                 <ProductDetail/>
               </Route>
-
-              <Route path="/result_product">
-                <SearchResultPage/>
+              <Route path="/aboutus">
+                <Aboutus/>
               </Route>
-
             </Switch>
           </div>
         </div>
--- stocky/src/Navbarwithoutsearch.js
+++ stocky/src/Navbarwithoutsearch.js
@@ -2,27 +2,18 @@ import { Link } from 'react-router-dom';
 import logo from './asset/img/logo.png';
 import React, {useState} from 'react';
 import { GoSearch } from "react-icons/go";
-import './Navbarwithsearch.css';
+import './Navbarwithoutsearch.css';
 const Navbarwithsearch = () => {
     return ( 
         <nav className="navbar"style={{
             color: "black", backgroundColor: "black", 
     }}>
     <img style={{ width: 80, height: 80 }} src={logo} alt="logo"/>
-    <Link to="/searchHome">
-    <div className= "whitebox">
-        
-        <div className="box">
-            <div className="searchcicon">
-                <GoSearch />
-            </div>
-        </div>
-    </div></Link>
+    
     <div className="links" >
-        <Link className="button" to="/aboutus" style={{
+        <Link className="button" to="/" style={{
             color: "white", backgroundColor: "red"
         }}>About us</Link>
-        
         <Link className="button" to="/" style={{
             color: "white", backgroundColor: "red"
         }}>Login</Link>
