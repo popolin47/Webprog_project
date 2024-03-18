de8d9e5d0193bc4058dff5716645d0df44f8d045
jajayjin@gmail.com <jajayjin@gmail.com>
2024-03-18 22:05:24 +0700

    พำิฟหำ

--- stocky/src/App.js
+++ stocky/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import './App.css';
+
 import Blogdetails from './Blogdetails';
 import Home from './Home';
 import Navbarwithoutsearch from './Navbarwithoutsearch';
