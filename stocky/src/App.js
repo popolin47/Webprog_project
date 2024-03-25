ce0d768510f42df35106e505bdb1cefd3dc2f373
RollTee <163039525+RollTee@users.noreply.github.com>
2024-03-25 21:46:26 +0700

    ssssss

--- stocky/src/App.js
+++ stocky/src/App.js
@@ -81,11 +81,12 @@ class App extends React.Component {
                 <Sidebar/>
                 <ProductManage/>
               </Route>
+
               <Route path="/login">
-              <Navbarwithoutsearch/>
+                <Navbarwithoutsearch/>
                 <Login/>
-              
               </Route>
+
               <Route path='/AddProduct'>
                 <Sidebar/>
                 <AddProduct/>
