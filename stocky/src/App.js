d008130ff7290472c3184fede62cffca6de44dbd
RollTee <163039525+RollTee@users.noreply.github.com>
2024-03-28 00:20:25 +0700

    dasd

--- stocky/src/App.js
+++ stocky/src/App.js
@@ -119,12 +119,11 @@ class App extends React.Component {
 
              
 
-           
               <Route path="/ProductSearchAdmin">
-                <Sidebar/>
+                {/* <Sidebar/> */}
                 <ProductSearchAdmin/>
               </Route>
-              
+
             </Switch>
           </div>
         </div>
