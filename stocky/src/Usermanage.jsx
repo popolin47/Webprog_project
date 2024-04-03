788e59a81d3d7d0aff10e808391137ddffa95dec
jajayjin@gmail.com <jajayjin@gmail.com>
2024-04-03 10:15:03 +0700

    update

--- stocky/src/Usermanage.jsx
+++ stocky/src/Usermanage.jsx
@@ -39,11 +39,11 @@ const Usermanage = () => {
       <div className="sm:ml-64 shadow-md">
         <div className="px-8">
           <h1 className="text-lg pb-5">User</h1>
-          <div className="flex-row">
+          <div className="flex flex-row ">
           <input type="password" name="pass" id="pass" onChange={handleChange}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
        focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
         dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Username' required />
-            <button type="submit" onClick={handleSearchSubmit} className= "pr-5 bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">
+            <button type="submit" onClick={handleSearchSubmit} className= "mx-4 bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">
               Search
             </button>
             <Link to="/searchuser">
