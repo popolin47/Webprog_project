a424284f9b492829722eb41e62c4432757ef26e2
jajayjin@gmail.com <jajayjin@gmail.com>
2024-04-22 09:53:17 +0700

    fe

--- stocky/src/Navbarwithoutsearch.js
+++ stocky/src/Navbarwithoutsearch.js
@@ -1,29 +1,41 @@
-import { Link } from 'react-router-dom';
 import logo from './asset/img/logo.png';
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoSearch } from "react-icons/go";
 import './Navbarwithoutsearch.css';
+import { Link, useLocation, useHistory} from 'react-router-dom';
 
 const Navbarwithsearch = () => {
-    return ( 
-        <nav className="navbar"style={{
-            color: "black", backgroundColor: "black", 
-    }}><Link to="/">
-        <img style={{ width: 80, height: 80 }} src={logo} alt="logo"/>
-    </Link>
-    
-    
-    <div className="links" >
-        <Link className="button rounded" to="/aboutus" style={{
-            color: "white", backgroundColor: "red"
-        }}>About us</Link>
-        <Link className="button rounded" to="/login" style={{
-            color: "white", backgroundColor: "red"
-        }}>Login</Link>
-       
- </div>
-</nav>
-     );
+    const location = useLocation();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const history = useHistory();
+    const handleLogout = () => {
+        localStorage.clear();
+        
+    };
+    useEffect(() => {
+        const userID = localStorage.getItem("AID");
+        if (userID !== null) {
+            setLoggedIn(true);
+        } else {
+            setLoggedIn(false);
+        }
+    }, []);
+
+    return (
+        <nav className="navbar" style={{ color: "black", backgroundColor: "black" }}>
+            <Link to="/">
+                <img style={{ width: 80, height: 80 }} src={logo} alt="logo" />
+            </Link>
+            <div className="links">
+                <Link className="button rounded" to="/aboutus" style={{ color: "white", backgroundColor: "red" }}>About us</Link>
+                {((location.pathname === "/searchHome" ) && loggedIn) ? (
+                    <Link className="button rounded " onClick={handleLogout} to="/"  style={{ color: "white", backgroundColor: "red" }}>Logout</Link>
+                ) : (
+                    <Link className="button rounded" to="/login" style={{ color: "white", backgroundColor: "red" }}>Login</Link>
+                )}
+            </div>
+        </nav>
+    );
 }
- 
+
 export default Navbarwithsearch;
--- stocky/src/Navbarwithsearch.js
+++ stocky/src/Navbarwithsearch.js
@@ -1,9 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory} from 'react-router-dom';
 import logo from './asset/img/logo.png';
-import React, {useState} from 'react';
+import React, {useEffect,useState} from 'react';
 import { GoSearch } from "react-icons/go";
 import './Navbarwithsearch.css';
 const Navbarwithsearch = () => {
+    const location = useLocation();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const handleLogout = () => {
+        localStorage.clear();
+       
+    };
+    useEffect(() => {
+        const userID = localStorage.getItem("AID");
+        if (userID !== null) {
+            setLoggedIn(true);
+        } else {
+            setLoggedIn(false);
+        }
+    }, []);
+
     return ( 
         <div className="navbar"><Link to="/">
         <img style={{ width: 80, height: 80 }} src={logo} alt="logo"/>
@@ -16,17 +31,17 @@ const Navbarwithsearch = () => {
             </div>
         </div>
     </div></Link>
-    <div className="links" >
-        <Link className="button rounded bg-pink" to="/aboutus" style={{
-            color: "white", backgroundColor: "red"
-        }}>About us</Link>
-        
-        <Link className="button rounded" to="/login" style={{
-            color: "white", backgroundColor: "red"
-        }}>Login</Link>
+    <div className="links">
+                <Link className="button rounded" to="/aboutus" style={{ color: "white", backgroundColor: "red" }}>About us</Link>
+                {((location.pathname === "/" || location.pathname === "/aboutus" ) && loggedIn) ? (
+                    <Link className="button rounded " onClick={handleLogout} to="/"  style={{ color: "white", backgroundColor: "red" }}>Logout</Link>
+                ) : (
+                    <Link className="button rounded" to="/login" style={{ color: "white", backgroundColor: "red" }}>Login</Link>
+                )}
+            </div>
        
  </div>
-</div>
+
      );
 }
  
